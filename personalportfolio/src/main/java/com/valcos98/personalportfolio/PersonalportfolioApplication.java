66c6445c5925021c590f3190034c1b5c72397b5b
Hector Cosgalla <valcos98@hotmail.com>
2023-08-23 15:42:23 -0600

    uploading all the stuff to start something

--- personalportfolio/src/main/java/com/valcos98/personalportfolio/PersonalportfolioApplication.java
+++ personalportfolio/src/main/java/com/valcos98/personalportfolio/PersonalportfolioApplication.java
@@ -3,7 +3,10 @@ package com.valcos98.personalportfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class PersonalportfolioApplication {
 
 	public static void main(String[] args) {
---
+++ personalportfolio/src/main/java/com/valcos98/personalportfolio/models/Technologies.java
@@ -0,0 +1,31 @@
+package com.valcos98.personalportfolio.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Technologies")
+public class Technologies {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "tech_name", length = 50)
+    private String name;
+
+    @Column(name = "years")
+    private int years;
+
+    public Technologies(String name, int years){
+        this.name = name;
+        this.years = years;
+    }
+}
