251517f61c6929085fef5c2375f3110131cc9471
Hector Cosgalla <valcos98@hotmail.com>
2023-08-24 20:38:02 -0600

    refactoring and creating CRUD

--- personalportfolio/src/main/java/com/valcos98/personalportfolio/controllers/MasterController.java
+++ personalportfolio/src/main/java/com/valcos98/personalportfolio/controllers/MasterController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,27 +24,48 @@ public class MasterController {
     public String getAllCV(Model model){
         List<Technologies> allTechs = techRepo.findAll();
         model.addAttribute("techs", allTechs);
-        return "front/full-cv";
+        return "back/landing-page";
     }
 
-    @GetMapping("/edit")
-    public String getEditPage(){
-        
+    @GetMapping("/edit/{id}")
+    public String getEditPage(@PathVariable Long id,Model model){
+        Technologies techToEdit = techRepo.getReferenceById(id);
+        model.addAttribute("technology", techToEdit);
         return "back/edit-cv";
     }
 
-    @PostMapping("/edit")
+    @PostMapping("/edit/{id}")
+    public String updateATech(@PathVariable Long id, Technologies techUpdated){
+        Technologies technology = techRepo.getReferenceById(id);
+        technology.setName(techUpdated.getName());
+        technology.setYears(techUpdated.getYears());
+        techRepo.save(technology);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteATech(@PathVariable Long id){
+        techRepo.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/upload")
+    public String getUploadPage(){
+        
+        return "back/upload-cv";
+    }
+
+    @PostMapping("/upload")
     public String postAllTheCVInfo(@RequestParam String techString){
         List<Technologies> listOfTechs = new ArrayList<>();
         String[] techsSplitted = techString.split("\\r\\n");
         for (String string : techsSplitted) {
             String[] singleTech = string.split(",");
-            System.out.println(singleTech[1]);
             Integer years = Integer.valueOf(singleTech[1]);
             Technologies technology = new Technologies(singleTech[0],years);
             listOfTechs.add(technology);
         }
         techRepo.saveAll(listOfTechs);
-        return "redirect:/edit";
+        return "redirect:/";
     }
 }
